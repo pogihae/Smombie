b7fd931fa05b36284d542e5abb4d276c02145c2e
pogihae <tir2986@naver.com>
2023-03-22 21:59:36 +0900

    feat: request permissions with dialog

--- app/src/main/java/com/example/smombie/MainActivity.kt
+++ app/src/main/java/com/example/smombie/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.smombie
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.ComponentName
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.ServiceConnection
 import android.content.pm.PackageManager
@@ -10,6 +14,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
 import android.provider.Settings
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -17,6 +22,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.smombie.analysis.AnalysisService
+import com.permissionx.guolindev.PermissionX
 
 // permission activity
 class MainActivity : AppCompatActivity() {
@@ -36,29 +42,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var startButton: Button
+    private val requestOverlayPermissionLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (Settings.canDrawOverlays(this).not()) {
+            finishWithReason("Overlay permission required")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 권한
-        if (!Settings.canDrawOverlays(this)) {
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                if (Settings.canDrawOverlays(this).not()) {
-                    finish()
-                }
-            }.launch(
-                Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:$packageName")
-                )
-            )
-        }
-        if (!allPermissionsGranted()) {
-            ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, RC_PERMISSIONS
-            )
-        }
+        requestPermissions()
 
         startButton = findViewById(R.id.start_button)
         startButton.setOnClickListener { bindAnalysisService() }
@@ -70,24 +64,58 @@ class MainActivity : AppCompatActivity() {
         unbindService(serviceConnection)
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == RC_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                //bindAnalysisService()
-            } else {
-                Toast.makeText(
-                    this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                finish()
+    private fun requestPermissions() {
+        val perms = mutableListOf(
+            Manifest.permission.CAMERA
+        ).apply {
+            if (Build.VERSION.SDK_INT >= 33) {
+                add(Manifest.permission.POST_NOTIFICATIONS)
             }
         }
+
+        // Runtime permissions
+        PermissionX.init(this)
+            .permissions(perms)
+            .explainReasonBeforeRequest()
+            .onExplainRequestReason { scope, deniedList ->
+                scope.showRequestReasonDialog(
+                    deniedList,
+                    "Core fundamental are based on these permissions",
+                    "OK", "Cancel")
+            }
+            .request { allGranted, _, _ ->
+                Log.d(TAG, "Finish permission request")
+                if (allGranted.not()) {
+                    finishWithReason("Core permissions are denied")
+                }
+
+                // Overlay permission
+                if (Settings.canDrawOverlays(this).not()) {
+                    Log.d(TAG, "Request overlay permission")
+                    AlertDialog.Builder(this)
+                        .setTitle("Request Overlay permission")
+                        .setMessage("Require overlays permission")
+                        .setNegativeButton("CANCEL") { _, _ ->
+                            finishWithReason("Overlay permission required")
+                        }
+                        .setPositiveButton("OK") { _, _ ->
+                            requestOverlayPermissionLauncher.launch(
+                                Intent(
+                                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                    Uri.parse("package:$packageName")
+                                )
+                            )
+                        }
+                        .create()
+                        .show()
+                }
+            }
+    }
+
+    private fun finishWithReason(reason: String) {
+        Toast.makeText(this, reason, Toast.LENGTH_LONG)
+            .show()
+        finish()
     }
 
     private fun bindAnalysisService() {
@@ -96,23 +124,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun allPermissionsGranted(): Boolean {
-        return Settings.canDrawOverlays(this) && REQUIRED_PERMISSIONS.all {
-            ContextCompat.checkSelfPermission(
-                baseContext, it
-            ) == PackageManager.PERMISSION_GRANTED
-        }
-    }
-
     companion object {
-        private const val RC_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = mutableListOf(
-            android.Manifest.permission.CAMERA
-        ).apply {
-            if (Build.VERSION.SDK_INT >= 33) {
-                add(android.Manifest.permission.POST_NOTIFICATIONS)
-            }
-        }.toTypedArray()
+        private const val TAG = "MainActivity"
 
     }
 }
